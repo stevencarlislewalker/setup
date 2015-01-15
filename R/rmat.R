d34d535e844b9d0175a6b72be3cbc827adbb05cb
stevencarlislewalker <scwalker@math.mcmaster.ca>
2015-01-15 15:29:43 -0500

    rmat

---
+++ R/rmat.R
@@ -0,0 +1,8 @@
+##' Random matrix
+##'
+##' @param rfunc an \code{r} function (e.g. \code{\link{rnorm}})
+##' @param nrow,ncol number of rows and columns
+##' @param ... parameters to \code{rfunc}
+##' @return a random matrix
+##' @export
+rmat <- function(rfunc, nrow = 1, ncol = 1, ...) rfunc(nrow*ncol, ...)
